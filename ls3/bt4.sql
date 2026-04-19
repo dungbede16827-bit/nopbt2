898a38fe77162c5b64de1ed34eac9b4844796a2a
dungbede16827-bit <dungbede16827@gmail.com>
2026-04-19 22:46:42 +0700

    Add files via upload

--- ls3/bt4.sql
+++ ls3/bt4.sql
@@ -1,7 +1,7 @@
 -- Cách 1: Xóa đơn hàng bị hủy
 DELETE FROM ORDERS
 WHERE Status = 'Canceled';
--- Cách 2: Soft Delete (chọn cách này)
+-- Cách 2: Soft Delete 
 -- Thêm cột
 ALTER TABLE ORDERS
 ADD IsDeleted BIT DEFAULT 0;
@@ -12,12 +12,12 @@ SET IsDeleted = 1
 WHERE Status = 'Canceled';
 
 
--- Truy vấn cho người dùng (không thấy đơn hủy)
+-- Truy vấn cho người dùng 
 SELECT *
 FROM ORDERS
 WHERE IsDeleted = 0;
 
--- Truy vấn cho kế toán (vẫn thấy đơn hủy)
+-- Truy vấn cho kế toán 
 SELECT *
 FROM ORDERS
 WHERE Status = 'Canceled';
